6db5c044defb9728448bf8469bdafaf7e54b6bca
Sahiru <sahiru906@gmail.com>
2024-07-17 22:19:08 +0530

    updated

--- components/animations/Dotted.tsx
+++ components/animations/Dotted.tsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { ReactNode } from 'react';
 
-const Dotted = ({ children }) => {
+type DottedProps = {
+  children: ReactNode;
+};
+
+const Dotted: React.FC<DottedProps> = ({ children }) => {
   return (
     <div className="w-full dark:bg-black bg-white dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center">
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       {children}
     </div>
-  )
-}
+  );
+};
 
-export default Dotted
+export default Dotted;
