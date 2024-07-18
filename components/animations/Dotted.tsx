a9baad15deaf308d343c8d4284cd310f8c718ae5
Sahiru <sahiru906@gmail.com>
2024-07-18 22:02:21 +0530

    updated

--- components/animations/Dotted.tsx
+++ components/animations/Dotted.tsx
@@ -6,8 +6,8 @@ type DottedProps = {
 
 const Dotted: React.FC<DottedProps> = ({ children }) => {
   return (
-    <div className="bg-dot-white w-full dark:bg-dot-white/[0.2] relative flex items-center justify-center">
-      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+    <div className="bg-dot-white/[0.2] w-full dark:bg-dot-white/[0.2] relative flex items-center justify-center">
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       {children}
     </div>
   //    <div className="w-full dark:bg-black dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center">
